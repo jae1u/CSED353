b638aad821e41f11dcbe9bc37cbe39a5dafd77ff
jaewonyu <jaewonyu@postech.ac.kr>
2025-03-01 12:09:51 +0000

    feat: Implement input validation and maintain consistency

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -4,28 +4,31 @@ using namespace std;
 ByteStream::ByteStream(const size_t capacity) : _capacity{capacity} {}
 
 size_t ByteStream::write(const string &data) {
-    const size_t input_length = _min(data.length(), remaining_capacity());
-    _stream += data.substr(0, input_length);
-    _written += input_length;
-    return input_length;
+    const size_t write_length = _min(data.length(), remaining_capacity());
+    _stream += data.substr(0, write_length);
+    _written += write_length;
+    return write_length;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
-string ByteStream::peek_output(const size_t len) const { return _stream.substr(0, len); }
+string ByteStream::peek_output(const size_t len) const {
+    const size_t peek_length = _min(len, buffer_size());
+    return _stream.substr(0, peek_length);
+}
 
 //! \param[in] len bytes will be removed from the output side of the buffer
 void ByteStream::pop_output(const size_t len) {
-    _popped += len;
-    _stream = _stream.substr(len, _stream.length());
+    const size_t pop_length = _min(len, buffer_size());
+    _popped += pop_length;
+    _stream = _stream.substr(pop_length, _stream.length());
 }
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    const size_t output_length = _min(len, buffer_size());
-    const string result = peek_output(output_length);
-    pop_output(output_length);
+    const string result = peek_output(len);
+    pop_output(len);
     return result;
 }
 
