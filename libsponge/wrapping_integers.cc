8df4a9fc44daff6ac0811b60874b66b0fee4c22f
jaewonyu <jaewonyu@postech.ac.kr>
2025-03-19 05:13:01 -0700

    feat: Implement Integer Wrapper for Assignment 2

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,22 +1,11 @@
 #include "wrapping_integers.hh"
 
-// Dummy implementation of a 32-bit wrapping integer
-
-// For Lab 2, please replace with a real implementation that passes the
-// automated checks run by `make check_lab2`.
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
-WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
-}
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) { return WrappingInt32(n) + isn.raw_value(); }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
@@ -29,6 +18,14 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t pow_2_32 = 1ul << 32;
+    uint64_t abs_seqno_mod_32 = (n - isn) & 0xffffffff;
+
+    if (checkpoint < static_cast<uint64_t>(abs_seqno_mod_32))
+        return abs_seqno_mod_32;
+
+    uint64_t block_offset = (checkpoint - abs_seqno_mod_32) / pow_2_32 * pow_2_32;
+    uint64_t lower = abs_seqno_mod_32 + block_offset;
+    uint64_t higher = abs_seqno_mod_32 + block_offset + pow_2_32;
+    return (higher - checkpoint) <= (1ul << 31) ? higher : lower;
 }
